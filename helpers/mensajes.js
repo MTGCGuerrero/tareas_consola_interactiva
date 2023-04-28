d8dc8180bbba0a0c4ab90dcf64cf4be435297bed
Manuel Tadeo Guerrero <mguerrero@roshka.com>
2023-04-28 11:49:37 -0400

    ajustes

--- helpers/mensajes.js
+++ helpers/mensajes.js
@@ -1,34 +1,38 @@
 require('colors');
 
 
-
 const mostrarMenu = () => {
 
-console.clear();
-console.log('==================================='.green);
-console.log('       Seleccione una opcion       '.green);
-console.log('===================================\n'.green);
+    return new Promise( resolve => {
 
+        console.clear();
+        console.log('=========================='.green);
+        console.log('  Seleccione una opción'.green);
+        console.log('==========================\n'.green);
 
-console.log(`${ '1.'.green } Crear tarea`);
-console.log(`${ '2.'.green } Listar tareas`);
-console.log(`${ '3.'.green } Listar tareas completadas`);
-console.log(`${ '4.'.green } Listar tareas pendientes`);
-console.log(`${ '5.'.green } Completar tarea(s)`);
-console.log(`${ '6.'.green } Borrar tarea`);
-console.log(`${ '0.'.green } Salir \n`);
+        console.log(`${ '1.'.green } Crear tarea`);
+        console.log(`${ '2.'.green } Listar tareas`);
+        console.log(`${ '3.'.green } Listar tareas completadas`);
+        console.log(`${ '4.'.green } Listar tareas pendientes`);
+        console.log(`${ '5.'.green } Completar tarea(s)`);
+        console.log(`${ '6.'.green } Borrar tarea`);
+        console.log(`${ '0.'.green } Salir \n`);
 
+        const readline = require('readline').createInterface({
+            input: process.stdin,
+            output: process.stdout
+        });
 
-const readline = require('readline').createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
+        readline.question('Seleccione una opción: ', (opt) => {
+            readline.close();
+            resolve(opt);
+        })
 
-readline.question('Seleccione una opción: ', (opt) => {
+    });
 
-    readline.close();
-    resolve(opt);
-})
+    
+
+}
 
 const pausa = () => {
 
@@ -44,12 +48,6 @@ const pausa = () => {
         })
     });
 }
-
-
-
-}
-
-
 
 
 module.exports = {
